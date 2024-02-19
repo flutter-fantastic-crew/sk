9484c52b6b7c15a175abfd6ccb3c57d7dc2a5848
ohMySunset <int4bute@gmail.com>
2024-02-19 18:08:13 +0900

    fix: 사이즈 수정

--- lib/widget/badge_widget.dart
+++ lib/widget/badge_widget.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 
 class BadgeWidget extends StatelessWidget {
-  const BadgeWidget({super.key});
+  final String text;
+
+  const BadgeWidget({super.key, required this.text});
 
   @override
   Widget build(BuildContext context) {
-    // 뱃지 디자인이 상익님 말처럼 안되네요.... 컹..
     return Container(
-      height: 25,
+      height: 20,
       padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 2.0),
       decoration: BoxDecoration(
         color: Colors.black38,
-        borderRadius: BorderRadius.circular(13.0),
+        borderRadius: BorderRadius.circular(50),
       ),
-      child: const Text(
-        '요약',
-        style: TextStyle(
-            color: Colors.white, fontSize: 13, fontWeight: FontWeight.bold),
+      child: Text(
+        text,
+        style: const TextStyle(
+            color: Colors.white, fontSize: 10, fontWeight: FontWeight.bold),
       ),
     );
   }
