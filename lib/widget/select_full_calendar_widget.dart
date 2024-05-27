f46988f7fcfdd8fc08119c9eb2438698a74e7d3d
ohMySunset <int4byte@gmail.com>
2024-05-27 23:38:00 +0900

    refactor: 일자 하나만 선택 가능한 달력으로 변경

--- lib/widget/select_full_calendar_widget.dart
+++ lib/widget/select_full_calendar_widget.dart
@@ -1,43 +1,96 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class SelectFullCalendarWidget extends StatelessWidget {
-  final int monthIndex;
+import '../view_model/add_history_view_model.dart';
 
-  const SelectFullCalendarWidget({super.key, required this.monthIndex});
+class SelectFullCalendarWidget extends StatefulWidget {
+  const SelectFullCalendarWidget({super.key});
+
+  @override
+  State<SelectFullCalendarWidget> createState() => _SelectFullCalendarWidget();
+}
+
+class _SelectFullCalendarWidget extends State<SelectFullCalendarWidget> {
+  final DateTime _firstDay =
+      DateTime(DateTime.now().year - 1, DateTime.now().month, 1);
+  final DateTime _lastDay =
+      DateTime(DateTime.now().year + 1, DateTime.now().month, 0);
+  late DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  Random random = Random();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    DateTime firstDayOfMonth = DateTime(DateTime.now().year, monthIndex + 1, 1);
-    // print(firstDayOfMonth);
-    // DateTime lastDayOfMonth = DateTime(DateTime.now().year, monthIndex + 2, 0);
-    DateTime today = DateTime.now();
-
-    bool isTodayInMonth = today.year == firstDayOfMonth.year &&
-        today.month == firstDayOfMonth.month;
-
-    return TableCalendar(
-      firstDay: DateTime(DateTime.now().year, 1, 1),
-      lastDay: DateTime(DateTime.now().year, 12, 31),
-      startingDayOfWeek: StartingDayOfWeek.monday,
-      focusedDay: isTodayInMonth ? today : firstDayOfMonth,
-      calendarFormat: CalendarFormat.month,
-      onDaySelected: (selectedDay, focusedDay) {},
-      calendarStyle: const CalendarStyle(
-        outsideDaysVisible: true,
-        todayDecoration: BoxDecoration(
-          color: Colors.transparent,
-        ),
-        todayTextStyle: TextStyle(
-          color: Colors.blueAccent,
-          fontWeight: FontWeight.w800,
-        ),
-      ),
-      headerStyle: const HeaderStyle(
-        formatButtonVisible: false,
-        leftChevronVisible: false,
-        rightChevronVisible: false,
-      ),
-    );
+    return FractionallySizedBox(
+        heightFactor: 0.66,
+        child: SizedBox(
+          height: (MediaQuery.of(context).size.height / 7) * 4.3,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(height: 20),
+              Container(
+                width: 60,
+                height: 6,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(3.0),
+                  color: const Color(0xFFE0E0E0),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              Expanded(
+                child: TableCalendar(
+                  firstDay: _firstDay,
+                  lastDay: _lastDay,
+                  focusedDay: _focusedDay,
+                  calendarFormat: CalendarFormat.month,
+                  selectedDayPredicate: (day) {
+                    return isSameDay(_selectedDay, day);
+                  },
+                  onDaySelected: (selectedDay, focusedDay) {
+                    context.read<AddHistoryViewModel>().setDate(selectedDay);
+                    setState(() {
+                      _selectedDay = selectedDay;
+                      _focusedDay = focusedDay;
+                    });
+                  },
+                  calendarStyle: CalendarStyle(
+                      outsideDaysVisible: true,
+                      todayDecoration: BoxDecoration(
+                        color: _selectedDay == null ||
+                                isSameDay(_selectedDay, DateTime.now())
+                            ? Colors.black
+                            : Colors.transparent,
+                        shape: BoxShape.circle,
+                      ),
+                      selectedDecoration: const BoxDecoration(
+                        color: Colors.black,
+                        shape: BoxShape.circle,
+                      ),
+                      selectedTextStyle: const TextStyle(
+                        color: Colors.white,
+                      ),
+                      todayTextStyle: TextStyle(
+                        color: _selectedDay == null ||
+                                isSameDay(_selectedDay, DateTime.now())
+                            ? Colors.white
+                            : Colors.black,
+                      )),
+                  headerStyle: const HeaderStyle(
+                    formatButtonVisible: false,
+                    titleCentered: true,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
